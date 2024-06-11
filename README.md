4c54fe1d7839bd25b8a69b7c083f7ed88d5f88c8
Helio Frota <00hf11@gmail.com>
2024-06-11 19:18:07 -0300

    other test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dpc
+
+[![ci](https://github.com/helio-frota/dpc/actions/workflows/ci.yaml/badge.svg)](https://github.com/helio-frota/dpc/actions/workflows/ci.yaml)
+
+> 'dpc' is a shorter name for 'duplicrabs' (which is a made-up word).
+
+Basic and not 100% accurate for finding duplicate code blocks in Rust projects.
+
--- src/test.rs
+++ src/test.rs
@@ -7,4 +7,12 @@ mod tests {
         let files = util::rust_files(".").expect("rust files not found.");
         assert_eq!(3, files.len());
     }
+
+    #[test]
+    fn test_get_content() {
+        let files = util::rust_files(".").expect("rust files not found.");
+        let path_as_str = files[0].to_string_lossy();
+        let content = util::content(&path_as_str);
+        assert!(content.unwrap().contains("use"));
+    }
 }
